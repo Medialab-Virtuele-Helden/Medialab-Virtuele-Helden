3fdf4a5d2d2f7fbd4310a25524667966cb673d34
TimWetzels <t.wetzels@onwego.nl>
2023-03-21 17:31:13 +0100

    Feature(Auth): Login/registratie systeem gemaakt

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome-test');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
