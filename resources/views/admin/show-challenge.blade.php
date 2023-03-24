ad927ec3ef9bedfc5ade0ce1fbabc9530dce2d6d
QuintyHR <70649864+QuintyHR@users.noreply.github.com>
2023-03-24 12:00:01 +0100

    Added the Challenge card including the styling

--- resources/views/admin/show-challenge.blade.php
+++ resources/views/admin/show-challenge.blade.php
@@ -17,7 +17,23 @@
 
                 <div class="row">
                     <div class="card shadow bg-white p-4">
-                        <h2 class="mb-0">Uitdaging</h1>
+                        <img src="{{ asset('images/challenge-example-image.png') }}" alt="Afbeelding vor de uitdaging" class="">
+                        <div>
+                            <div>
+                                <h1 class="mb-1 mt-3">Uitdaging</h1>
+                                <h4>Zorg dat er totaal 300 antwoorden worden gegeven.</h4>
+                            </div>
+                            <div>
+                                <h4 class="mt-5"><b>Prijs:</b> De 10 meest actieve gebruikers worden uitgenodigd voor een exclusieve borrel samen met Roel!</h4>
+                            </div>
+                        </div>
+                        <div class="progress mt-5">
+                            <div class="progress-bar d-flex flex-row justify-content-between" role="progressbar" style="width: 90%">
+                                <h4 class="o-text"><b class="float-start ms-3 mt-2" style="">270/300</b></h4>
+
+                                <h4 class="o-text"><b class="float-end me-3 mt-2">90%</b></h4>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -25,6 +41,9 @@
             <div class="col-3">
                 <div class="card shadow bg-white p-4 h-100">
                     <h2 class="mb-0">Leaderboard</h1>
+                    <div>
+
+                    </div>
                 </div>
             </div>
         </div>
