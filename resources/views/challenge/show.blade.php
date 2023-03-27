d091828d8b3c2d23dcfbd82fd4d5a75060ae84bd
Sara van den Bos <sara.vandenbos@iodigital.com>
2023-03-27 22:09:31 +0200

    fix(show.blade.php): changed $challenges to $challenge

--- resources/views/challenge/show.blade.php
+++ resources/views/challenge/show.blade.php
@@ -8,14 +8,14 @@
   <div class="card-body">
  
         <div class="card-body">
-        <h5 class="card-title">Title : {{ $challenges->title }}</h5>
-        <p class="card-text">Organiser : {{ $challenges->organiser }}</p>
-        <p class="card-text">Status : {{ $challenges->status }}</p>
-        <p class="card-text">Description : {{ $challenges->description }}</p>
-        <p class="card-text">Start Date : {{ $challenges->start_date }}</p>
-        <p class="card-text">End Daet : {{ $challenges->end_date }}</p>
-        <p class="card-text">Goal : {{ $challenges->challenge_goal }}</p>
-        <p class="card-text">Reward : {{ $challenges->reward }}</p>
+        <h5 class="card-title">Title : {{ $challenge->title }}</h5>
+        <p class="card-text">Organiser : {{ $challenge->organiser }}</p>
+        <p class="card-text">Status : {{ $challenge->status }}</p>
+        <p class="card-text">Description : {{ $challenge->description }}</p>
+        <p class="card-text">Start Date : {{ $challenge->start_date }}</p>
+        <p class="card-text">End Daet : {{ $challenge->end_date }}</p>
+        <p class="card-text">Goal : {{ $challenge->challenge_goal }}</p>
+        <p class="card-text">Reward : {{ $challenge->reward }}</p>
 
   </div>
        
