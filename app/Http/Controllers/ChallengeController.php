5aa5a2a3187c35087e6f57df4d15c1f78c23f625
Dancingkoalaa <tim2001.tw@gmail.com>
2023-03-27 13:04:01 +0200

    feat(Crud): add auth, findorfail and base for validation

--- app/Http/Controllers/ChallengeController.php
+++ app/Http/Controllers/ChallengeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -20,11 +21,17 @@ class ChallengeController extends Controller
     }
     public function create(): View
     {
+        if (Auth::check()) {
         return view('challenge.create');
+        }
+        abort(401);
     }
 
     public function store(Request $request): RedirectResponse
     {
+        // $validated = $request->validate([
+            //information
+        // ]);
         $input = $request->all();
         Challenge::create($input);
         return redirect('challenge')->with('flash_message', 'Challenge Added!');
@@ -32,28 +39,38 @@ class ChallengeController extends Controller
 
     public function show(string $id): View
     {
-        $challenge = Challenge::find($id);
+        $challenge = Challenge::findOrFail($id);
         return view('challenges.show')->with('challenges', $challenge);
     }
  
     public function edit(string $id): View
     {
-        $challenge = Challenge::find($id);
+        if (Auth::check()) {
+        $challenge = Challenge::findOrFail($id);
         return view('challenges.edit')->with('challenges', $challenge);
+        }
+        abort(401);
     }
  
     public function update(Request $request, string $id): RedirectResponse
     {
-        $challenge = Challenge::find($id);
+        $challenge = Challenge::findOrFail($id);
+        // $validated = $request->validate([
+            //information
+        // ]);
         $input = $request->all();
         $challenge->update($input);
         return redirect('challenge')->with('flash_message', 'challenge Updated!');  
     }
  
-    
-    public function destroy(string $id): RedirectResponse
+    //Up for discussion
+    /* public function destroy(string $id): RedirectResponse
     {
-        hallenge::destroy($id);
+        if (Auth::check()) {
+        Challenge::destroy($id);
         return redirect('challenge')->with('flash_message', 'Challenge deleted!');
+        }
+        abort(401);
     }
+    */ 
 }
